f3b8a0fc735178c64c2e7f7c80897914069ce502
Mrinal <dmrinal626@gmail.com>
2022-06-02 04:13:51 +0530

    Time: 70 ms (5.40%), Space: 14 MB (11.76%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,33 @@
+class Solution:
+    
+    def __init__(self):
+        self.cache = {}
+    
+#     def climbStairs(self, n: int) -> int:
+#         if n == 1 or n == 2:
+#             return n
+        
+#         if n in self.cache:
+#             return self.cache[n]
+        
+#         sums = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+#         self.cache[n] = sums
+#         return sums
+        
+    def climbStairs(self,n:int) -> int:
+        if n <= 2:
+            return n
+        res = [0 for _ in range(n + 1)]
+        
+        res[0] = 1
+        res[1] = 1
+        res[2] = 2
+        
+        for i in range(2,n+1,2):
+            res[i] = res[i - 1] + res[i - 2]
+            if i < n:
+                res[i + 1] = res[i] + res[i - 1]
+        
+        print(res)
+        return res[n]
