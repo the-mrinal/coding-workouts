bad7bf4c0feae79562a834e362d8b9a7207849a9
Mrinal <dmrinal626@gmail.com>
2022-06-02 20:05:12 +0530

    Time: 678 ms (5.04%), Space: 18.3 MB (99.81%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,26 +1,83 @@
+
+
+# class Solution:
+#     def leadsToDestination(self, n: int, edges: List[List[int]], src: int, dest: int) -> bool:
+
+#             adjMap = defaultdict(list)
+
+#             for start,end in edges:
+#                 adjMap[start].append(end)
+
+#             visited = set()
+            
+#             def dfs(v):
+#                 if v in visited:
+#                     return False
+                
+#                 if not adjMap[v]:
+#                     return v == dest
+                
+#                 visited.add(v)
+#                 for neigh in adjMap[v]:
+#                     if not dfs(neigh):
+#                         return False
+                
+#                 visited.remove(v)
+#                 return True
+            
+#             return dfs(src)
+
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], src: int, dest: int) -> bool:
+#         graph = defaultdict(set)
+#         for v1, v2 in edges:
+#             graph[v1].add(v2)
 
-            adjMap = defaultdict(list)
+#         stack = [(source,[source])] # stack for (vertex and path)
+
+#         while stack:
+#             node,path = stack.pop(-1)
+#             if node not in graph: 
+#                     if node != destination:  #ending point in a path
+#                         return False
+#                     else:
+#                         continue
+
+#             for next_node in graph[node]:
+#                 if next_node in path:
+#                     return False # circle detected
+#                 else:
+#                     stack.append((next_node,path+[next_node]))
+#         return True
+
+            adjMap = defaultdict(set)
 
             for start,end in edges:
-                adjMap[start].append(end)
+                adjMap[start].add(end)
 
-            visited = set()
-            
-            def dfs(v):
-                if v in visited:
-                    return False
-                
-                if not adjMap[v]:
-                    return v == dest
-                
-                visited.add(v)
-                for neigh in adjMap[v]:
-                    if not dfs(neigh):
+            st = [(src,[src])]
+
+            while st:
+                v,path = st.pop(-1)
+
+                if v not in adjMap:
+                    if v != dest:
+                        print('dffe',v,dest)
                         return False
-                
-                visited.remove(v)
-                return True
+                    else:
+                        continue
+
+                for neigh in adjMap[v]:
+                    if neigh in path:
+                        print('fe')
+                        return False
+                    else:
+                        st.append((neigh,path + [neigh]))
+
+            return True
             
-            return dfs(src)
+        
+        
+        
+        
+        
