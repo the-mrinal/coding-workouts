65777520e48ff4454e2d35da0854206c794ade20
Mrinal <dmrinal626@gmail.com>
2022-06-04 17:06:56 +0530

    Time: 85 ms (61.56%), Space: 14.4 MB (45.52%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,20 +1,24 @@
 class Solution:
-    def __init__(self):
-	    self.dict = []
     def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+
         def create_board(state):
             ans = []
-            for i in state:
-                ans.append("".join(i))
+            for row in state:
+                ans.append(''.join(row))
+
             return ans
 
-        def helper(row,diagonals,antidiagonals,cols,state):
+
+
+        def backtrack(row,cols,diagonals,antidiagonals,state):
             if row == n:
-                return self.dict.append(create_board(state))
-            soln = []
+                result.append(create_board(state))
+                return
+            
             for col in range(n):
                 diag = row - col
-                antidiag = col + row
+                antidiag = row + col
 
                 if col in cols or diag in diagonals or antidiag in antidiagonals:
                     continue
@@ -24,25 +28,21 @@ class Solution:
                 antidiagonals.add(antidiag)
                 state[row][col] = 'Q'
 
-                helper(row + 1,diagonals,antidiagonals,cols,state)
+                backtrack(row + 1,cols,diagonals,antidiagonals,state)
+
 
                 cols.remove(col)
                 diagonals.remove(diag)
                 antidiagonals.remove(antidiag)
                 state[row][col] = '.'
-        
+
         state = []
         for i in range(n):
             state.append([])
             for j in range(n):
-                state[-1].append('.')
-
-        helper(0,set(),set(),set(),state)
-        return self.dict
-
-    
-	
+                 state[-1].append('.')	
 
 
-		
-		
+        backtrack(0,set(),set(),set(),state)
+
+        return result
