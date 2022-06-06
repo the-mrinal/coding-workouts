202264e4d9e5c1a5371be951c533798f6f421abd
Mrinal <dmrinal626@gmail.com>
2022-06-06 23:40:12 +0530

    Time: 5999 ms, Memory: 123.7 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -6,38 +6,6 @@ class Edge:
 	
 	def __lt__(self,other):
 		return self.cost < other.cost
-
-class UnionFind:
-    def __init__(self,size):
-        self.root = [i for i in range(size)]
-        self.rank = [1 for _ in range(size)]
-        self.count = size
-    
-    def find(self,x):
-        if self.root[x] == x:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    
-    def union(self,a,b):
-        rootA = self.find(a)
-        rootB = self.find(b)
-        
-        if rootA == rootB:
-            return
-        
-        if self.rank[rootA] > self.rank[rootB]:
-            self.root[rootB] = rootA
-        elif self.rank[rootA] < self.rank[rootB]:
-            self.root[rootA] = rootB
-        else:
-            self.root[rootB] = rootA
-            self.rank[rootA] += 1
-        
-        self.count -= 1
-    
-    def isConnected(self,a,b):
-        return self.find(a) == self.find(b)
     
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
@@ -46,25 +14,28 @@ class Solution:
             xj,yj = points[b]
             return abs(xi - xj) + abs(yi - yj)
 		
-        n = len(points)
         
         min_heap = []
-        for j in range(n):
-            for i in range(j+1,n):
-                edge = Edge(j,i,cost(j,i))
-                min_heap.append(edge)
+        n = len(points)
+        for i in range(1,n):
+            edge = Edge(0,i,cost(0,i))
+            min_heap.append(edge)
         
         heapq.heapify(min_heap)
-        uf = UnionFind(n)
-        count = n - 1
+        visited = set()
+        visited.add(0)
         min_cost = 0
+        
+        count = n - 1 
         while min_heap and count > 0:
             edge = heapq.heappop(min_heap)
             
-            if not uf.isConnected(edge.a,edge.b):
-                uf.union(edge.a,edge.b)
+            if edge.b not in visited:
                 min_cost += edge.cost
-                
+                visited.add(edge.b)
+                for neigh in range(n):
+                    if neigh not in visited:
+                        heapq.heappush(min_heap,Edge(edge.b,neigh,cost(edge.b,neigh)))
                 count -= 1
 
         return min_cost
