da2de11af274cd17d2f5e8cd542b33a80e74a7c3
Mrinal <dmrinal626@gmail.com>
2022-06-06 21:56:14 +0530

    Time: 1337 ms, Memory: 15.3 MB - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,39 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m = len(grid) - 1
-        n = len(grid[0]) - 1
-        
-        
-        if grid[0][0] == 1 or grid[m][n] == 1:
+        n = len(grid)
+        if grid[0][0] == 1:
             return -1
-        
-        directions = [[1,0],[0,1],[-1,0],[0,-1],[-1,-1],[1,1],[-1,1],[1,-1]]
-        
+        directions = [[0,1],[1,0],[0,-1],[-1,0],[-1,-1],[1,1],[-1,1],[1,-1]]
         que = deque([[0,0,1]])
-        
-        def findNeigbors(i,j):
-            res = []
-            for r,c in directions:
-                n_r = r + i
-                n_c = c + j
-                if n_r < 0 or n_r > m or n_c < 0 or n_c > n or grid[n_r][n_c] != 0:
-                    continue
-                
-                res.append([n_r,n_c])
-            return res
-        seen = set()
+        visited = set((0,0))
         while que:
-            r,c,dist = que.popleft()
-            print(r,c,dist)
-            if [r,c] == [n,n]:
-                return dist
-            
-            for n_r,n_c in findNeigbors(r,c):
-                if (n_r,n_c) not in seen:
-                    que.append([n_r,n_c,dist + 1])
-                    seen.add((n_r,n_c))
-        
+            a,b,cost = que.popleft()
+            if a == n-1 and b == n-1:
+                return cost 
+
+    
+            for n_a,n_b in directions:
+                x = a + n_a
+                y = b + n_b
+
+                if x >= 0 and x < n and y >= 0 and y < n and grid[x][y] == 0 and (x,y) not in visited:
+                        que.append([x,y,cost + 1])
+                        visited.add((x,y))
+
+
+
         return -1
-            
-            
-            
