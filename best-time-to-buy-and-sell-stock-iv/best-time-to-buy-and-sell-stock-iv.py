ed904ba9aafbaa69cd77027a73711f106c564481
Mrinal <dmrinal626@gmail.com>
2022-06-07 23:49:21 +0530

    Time: 247 ms, Memory: 24 MB - LeetHub

--- best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -4,13 +4,13 @@ class Solution:
         dp = [[[0]*2 for _ in range(k+1)] for _ in range(n+1)]
 
         for i in range(n-1,-1,-1):
-            for j in range(1,k+1):
+            for j in range(k-1,-1,-1):
                 for isHolding in range(2):
                     doNothing = dp[i+1][j][isHolding]
                     if isHolding == 0:
                         doSomething = -prices[i] + dp[i+1][j][1]
                     else:
-                        doSomething = prices[i] + dp[i+1][j - 1][0]
+                        doSomething = prices[i] + dp[i+1][j + 1][0]
                     dp[i][j][isHolding] = max(doNothing,doSomething)
         
-        return dp[0][k][0]
+        return dp[0][0][0]
