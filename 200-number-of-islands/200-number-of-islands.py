4f2ee80863da3f64f832261fd9e6f03ac3128eee
Mrinal <dmrinal626@gmail.com>
2022-06-07 21:52:43 +0530

    Time: 591 ms (15.50%), Space: 20.6 MB (34.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,47 +1,47 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-        
+class UnionFind:
+    def __init__(self,grid):    
         m = len(grid)
         n = len(grid[0])
-        islands = 0
-        
-#         space = []
-#         for i in range(m):
-#             space.append([])
-#             for j in range(n):
-#                 space[i].append(False)
-        
-        
-#         def connectIsland(queue):
-#             while queue:
-#                 r_c,c_c = queue.popleft()
-#                 for r_d,c_d in directions:
-#                     r_n = r_c + r_d
-#                     c_n = c_c + c_d
-                    
-#                     if (r_n < 0 or r_n >= m or c_n < 0 or c_n >= n or space[r_n][c_n] == True or grid[r_n][c_n] == "0"):
-#                         continue
-                    
-#                     space[r_n][c_n] =  True
-#                     queue.append([r_n,c_n])
-                        
-        
-        
-        def dfs(dfsgrid,r,c):
-            if (r < 0 or r>= m or c<0 or c>=n or dfsgrid[r][c] == "0"):
-                return
-            dfsgrid[r][c] = "0"
-            dfs(dfsgrid,r - 1,c)
-            dfs(dfsgrid,r + 1,c)
-            dfs(dfsgrid,r,c - 1)
-            dfs(dfsgrid,r,c + 1)
+        self.count = sum(grid[i][j]=='1' for i in range(m) for j in range(n))
+        self.root = [i for i in range(m*n)]
+        self.rank = [0 for _ in range(m*n)]
 
-        
+    def find(self,x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self,a,b):
+        rootA = self.find(a)
+        rootB = self.find(b)
+
+        if rootA != rootB:
+            if self.rank[rootA] > self.rank[rootB]:
+                    self.root[rootB] = rootA
+            elif self.rank[rootA] < self.rank[rootB]:
+                    self.root[rootA] = rootB
+            else:
+                self.root[rootB] = rootA
+                self.rank[rootA] += 1
+            self.count -= 1
+	
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        uf = UnionFind(grid)
+        directions = [[1,0],[0,1]]
+        print(uf.count)
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
-                    islands += 1
-                    dfs(grid,i,j)
-        
-        return islands
+                if grid[i][j] == '0':
+                    continue
+                index = i*n + j
+                if j < n-1 and grid[i][j+1] == '1':
+                    uf.union(index, index+1)
+                if i < m-1 and grid[i+1][j] == '1':
+                    uf.union(index, index+n)
+
+        print(uf.count)
+        return uf.count
