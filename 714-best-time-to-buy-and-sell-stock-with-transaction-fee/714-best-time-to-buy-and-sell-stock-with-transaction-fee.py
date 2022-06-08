86a897183f3df5557ca6df17ef180fe64d7a847d
Mrinal <dmrinal626@gmail.com>
2022-06-09 03:50:16 +0530

    Time: 1554 ms (20.48%), Space: 23.1 MB (31.34%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,17 +1,34 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int], fee: int) -> int:
+#         n = len(prices)
+#         @lru_cache
+#         def dp(i,isHolding):
+#             if i == n:
+#                 return 0
+
+#             doNothing = dp(i+1,isHolding)
+#             doSomething = None
+#             if isHolding == 1:
+#                 doSomething = -fee + prices[i] + dp(i+1,0)
+#             else:
+#                 doSomething = -prices[i] + dp(i+1,1)
+
+#             return max(doSomething,doNothing)	
+#         return dp(0,0)
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        @lru_cache
-        def dp(i,isHolding):
-            if i == n:
-                return 0
-
-            doNothing = dp(i+1,isHolding)
-            doSomething = None
-            if isHolding == 1:
-                doSomething = -fee + prices[i] + dp(i+1,0)
-            else:
-                doSomething = -prices[i] + dp(i+1,1)
-
-            return max(doSomething,doNothing)	
-        return dp(0,0)
+        dp = [[0]*2 for _ in range(n + 1)]
+        for i in range(n-1,-1,-1):
+            for isHolding in range(2):
+                doNothing = dp[i + 1][isHolding]
+                doSomething = None
+                if isHolding:
+                    doSomething =  -fee + prices[i] + dp[i+1][0]
+                else:
+                    doSomething = - prices[i] + dp[i+1][1]
+            
+                dp[i][isHolding] = max(doSomething,doNothing)
+        
+        return dp[0][0]
+        
