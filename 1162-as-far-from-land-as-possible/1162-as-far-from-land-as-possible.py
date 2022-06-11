53c0393f21de409b59413f17eff429c33666bff8
Mrinal <dmrinal626@gmail.com>
2022-06-11 13:50:33 +0530

    Time: 1136 ms (20.02%), Space: 14.6 MB (81.50%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,17 +1,26 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-            m,n = len(grid), len(grid[0])
-            q = deque([(i,j) for i in range(m) for j in range(n) if grid[i][j] == 1])    
-            if len(q) == m * n or len(q) == 0: return -1
-            level = 0
-            while q:
-                size = len(q)
-                for _ in range(size):
-                    i,j = q.popleft()
-                    for x,y in [(1,0), (-1, 0), (0, 1), (0, -1)]:
-                        xi, yj = x+i, y+j
-                        if 0 <= xi < m and 0 <= yj < n and grid[xi][yj] == 0:
-                            q.append((xi, yj))
-                            grid[xi][yj] = 1
-                level += 1
-            return level-1
+        n = len(grid)
+        que = deque([(i,j) for i in range(n) for j in range(n) if grid[i][j] == 1])
+        
+        if len(que) == n*n or len(que) == 0: return -1
+        
+        level = 0
+        
+        while que:
+            size = len(que)
+            
+            for _ in range(size):
+                x,y = que.popleft()
+                
+                for n_x,n_y in [[1,0],[0,1],[-1,0],[0,-1]]:
+                    u_x = n_x + x
+                    u_y = n_y + y
+                    
+                    if u_x >=0 and u_x < n and u_y >= 0 and u_y < n and grid[u_x][u_y] == 0:
+                        que.append((u_x,u_y))
+                        grid[u_x][u_y] = 1
+                
+            level += 1
+        
+        return level - 1
