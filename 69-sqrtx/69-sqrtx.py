daca81830ea03ea881dec4adde56060d4a76828e
Mrinal <dmrinal626@gmail.com>
2022-06-11 11:02:59 +0530

    Time: 69 ms (34.05%), Space: 13.9 MB (10.41%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,38 +1,24 @@
 class Solution:
-    
-        # def mySqrt(self,x: int) -> int:
-        #     left, right = 0, x
-        #     while left < right:
-        #         mid = left + (right - left) // 2
-        #         if mid * mid <= x:
-        #             left = mid + 1
-        #         else:
-        #             right = mid
-        #     return left - 1
-
-    
     def mySqrt(self, x: int) -> int:
-        if x < 2:
+        if x <= 1:
             return x
         
         
-        def check(mid):
-            t = mid * mid
-            if t > x:
-                return True # ie, go to left search space
-            else:
-                return False
-            
         
+        def isFeasible(mid):
+            if mid * mid > x:
+                return True
+            return False
         
         left = 2
-        right = x
+        right = x 
+        
         while left < right:
             mid = left + (right - left) // 2
             
-            if check(mid):
+            if isFeasible(mid):
                 right = mid
             else:
                 left = mid + 1
-        
+            
         return left - 1
