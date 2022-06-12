1558fa8d57ec5ffd9c53231fbac6ce68883c68a0
Mrinal <dmrinal626@gmail.com>
2022-06-12 23:32:14 +0530

    Time: 43 ms (78.96%), Space: 14 MB (10.80%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.py
+++ 441-arranging-coins/441-arranging-coins.py
@@ -1,10 +1,31 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
 
-        sums = 0
-        for i in range(n + 1):
-            sums += i
-            if sums == n:
-                return i
-            elif sums > n:
-                return i - 1
+        # sums = 0
+        # for i in range(n + 1):
+        #     sums += i
+        #     if sums == n:
+        #         return i
+        #     elif sums > n:
+        #         return i - 1
+        si = 0
+        ei = n
+        
+        
+        def isFeasible(mid):
+            
+            t = int(mid*(mid+1)/2)
+            if t < n:
+                return False
+            else:
+                return True
+        
+        while si < ei:
+            mid = si + (ei - si)//2
+            
+            if isFeasible(mid):
+                ei = mid
+            else:
+                si = mid + 1
+            
+        return si if int(si*(si+1)/2) == n else si - 1
