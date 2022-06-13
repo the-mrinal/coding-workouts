9c055a96a8ced09ad9b4cc92c10da2a1523566f7
Mrinal <dmrinal626@gmail.com>
2022-06-13 19:34:30 +0530

    Time: 141 ms (9.52%), Space: 14.9 MB (60.15%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -24,9 +24,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        curr = [0]*(n)
+        curr = [0]*(n-1)
         prev = triangle[n-1]
         for level in range(n-2,-1,-1):
+            # print(curr)
             for i in range(level + 1):
                 curr[i] = triangle[level][i]+min(prev[i],prev[i+1])
             curr,prev = [0]*(level),curr
