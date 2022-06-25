0eceea88a814296d190cba4d9165b927eb0d4d6d
Mrinal <dmrinal626@gmail.com>
2022-06-25 21:02:09 +0530

    Time: 196 ms, Memory: 15.7 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,20 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        src = 0
-        dest = len(graph) - 1
-        
-        que = deque([[src,[src]]])
-
-
-        ans = []
-        
-
-        while que:
-            curr,path = que.popleft()
-
-            if curr == dest:
-                ans.append(path.copy())
+        stack = [[0,[0]]]
+        n = len(graph)
+        res = []
+        while stack:
+            curr,path = stack.pop()
+            if curr == n - 1:
+                res.append(path.copy())
                 continue
-            for b in graph[curr]:
-                que.append([b,path + [b]])
-        return ans
+
+            for neigh in graph[curr]:
+                stack.append([neigh,path + [neigh]])
+
+        return res
