28c579a620fc46dde41e108639765f2a8a73130f
Mrinal <dmrinal626@gmail.com>
2022-06-26 02:59:39 +0530

    Time: 578 ms, Memory: 15 MB - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,22 +1,16 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # we will have to make 2 queues - prev and curr
-        print(src,dst,k)
-        prev = [float('inf') for _ in range(n)]
-        curr = [float('inf') for _ in range(n)]
-        
+
+        prev = [float('inf')] * n
+        curr = [float('inf')] * n
+
         prev[src] = 0
-        
+
         for i in range(1,k + 2):
-            
             curr[src] = 0
-            
-            for s,d,dist in flights:
-                if curr[d] > prev[s] + dist:
-                    curr[d] = prev[s] + dist
-            print(curr)
+            for flight in flights:
+                prev_flight,curr_flight,cost = flight
+                if prev[prev_flight] < float('inf'):
+                    curr[curr_flight] = min(curr[curr_flight],prev[prev_flight] + cost)
             prev = curr.copy()
-            
-        return prev[dst] if prev[dst] < float('inf') else -1
-            
-            
+        return -1 if curr[dst] == float('inf') else curr[dst]
