37819f6236a9820d550d5f30c10ad4d7901262c4
Mrinal <dmrinal626@gmail.com>
2022-06-27 19:14:58 +0530

    Time: 859 ms (29.32%), Space: 15.3 MB (50.40%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,8 +1,40 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         self.caching = {}
+#         def dfs(n):
+#             if n >= len(nums) - 1:
+#                 return True
+            
+#             if n not in self.caching:
+#                 self.caching[n] = False
+#                 for i in range(1,nums[n]+1):
+#                     self.caching[n] = dfs(n + i)
+#                     if self.caching[n]:
+#                         return True
+#             return self.caching[n]
+#         return dfs(0)
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        last_index = len(nums) - 1
-        for i in range(len(nums) -2,-1,-1):
-            if (i+nums[i]) >= last_index:
-                last_index = i
+        lastIndex = len(nums) - 1
+        for i in range(len(nums)-2,-1,-1):
+            if (i + nums[i]) >= lastIndex:
+                lastIndex = i
+            
+        return lastIndex == 0
         
-        return last_index == 0
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
