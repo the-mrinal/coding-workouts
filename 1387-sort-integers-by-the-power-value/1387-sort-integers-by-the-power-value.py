1abed93d40465b29e895fc7bc073ecdd0a2f2e4e
Mrinal <dmrinal626@gmail.com>
2022-07-08 03:28:47 +0530

    Time: 247 ms (76.26%), Space: 14.4 MB (36.58%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,28 @@
+class Solution:
+
+        
+    
+    def countSteps(self,n):
+        if n in self.dic:
+            return self.dic[n]
+        if n % 2:
+            self.dic[n] = self.countSteps(3 * n + 1) + 1
+        else:
+            self.dic[n] = self.countSteps(n // 2) + 1
+        return self.dic[n]    
+
+        
+    
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        self.dic = {1:0}
+        for i in range(lo,hi+1):
+            self.countSteps(i)
+        lst = [(self.dic[i],i) for i in range(lo,hi+1)]
+        heapq.heapify(lst)
+
+        for i in range(k):
+            ans = heapq.heappop(lst)
+
+        return ans[1]    
+
+        
