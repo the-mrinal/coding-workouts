1b19862b3847dc25faea32592f49b9601280ee7c
Mrinal <dmrinal626@gmail.com>
2022-07-12 01:04:05 +0530

    Time: 31 ms (94.52%), Space: 14.9 MB (72.46%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -1,17 +1,26 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         index = self.findMountain(mountain_arr)
         n = mountain_arr.length()
-        indexA = self.binary_search(mountain_arr,0,index,target)
-        indexB = self.binary_search(mountain_arr,index,n - 1,target)
+        indexA = self.orderAgnosticBSearch(mountain_arr,0,index,target)
+        indexB = self.orderAgnosticBSearch(mountain_arr,index,n - 1,target)
+        
         return min(indexA,indexB) if min(indexA,indexB) < float('inf') else -1 
     
     def findMountain(self,nums):
         # nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]
         
         def checkFeasible(mid):
-            midElem =  nums.get(mid)
-            if midElem > nums.get(mid - 1): # we have got asc order in our hand.
+            midElem = nums.get(mid)
+            if  midElem > nums.get(mid - 1): # we have got asc order in our hand.
                 if midElem < nums.get(mid + 1):
                     return False # left mid + 1
                 else:
@@ -31,23 +40,29 @@ class Solution:
         
         return si
     
-    def binary_search(self,nums,si,ei,key):
-        
-        isAsc = nums.get(si) <= nums.get(ei)
-        while si <= ei:
-            mid = si + (ei - si)//2
+    def orderAgnosticBSearch(self,nums,si,ei,target):
+        start = nums.get(si)
+        end = nums.get(ei)
+        def checkFeasible(mid):
             midElem = nums.get(mid)
-            if midElem == key:
-                return mid		
-
-            if isAsc:
-                if midElem < key:
-                    si = mid + 1
+            if start < midElem or end > midElem: # asc order
+                if midElem >= target:
+                    return True
                 else:
-                    ei = mid - 1
+                    return False
+            elif start > midElem or end < start: # dsc ordr
+                if midElem <= target:
+                    return True
+                else:
+                    return False
             else:
-                if midElem < key:
-                    ei = mid - 1
-                else:
-                    si = mid + 1
-        return float('inf')
+                return True
+        
+        while si < ei:
+            mid = si + (ei - si)//2
+            if checkFeasible(mid):
+                ei = mid
+            else:
+                si = mid + 1
+        
+        return si if nums.get(si) == target else float('inf')
