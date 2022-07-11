f6e10b97612e95f260f4474cfe0fac82bb4e77f5
Mrinal <dmrinal626@gmail.com>
2022-07-11 22:17:55 +0530

    Time: 40 ms (95.60%), Space: 15.7 MB (14.25%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,26 +1,24 @@
 class Solution:
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m = len(s1) 
-        n = len(s2)
-        l = len(s3)
-        if m + n != l:
-            return False
-        
-        
-        table = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
-        
-        for i in range(m + 1):
-            for j in range(n + 1):
-                if i==0 and j==0:
-                    table[i][j] = True
-                elif i == 0:
-                    table[i][j] = table[i][j-1] and s2[j-1] == s3[i+j-1]
-                elif j == 0:
-                    table[i][j] = table[i-1][j] and s1[i-1] == s3[i+j-1]
-                else:
-                    table[i][j] = (table[i-1][j] and s1[i-1] == s3[i+j-1]) or (table[i][j-1] and s2[j-1] == s3[i+j-1])
-            
-        
-        
-        
-        return table[m][n]
+    def isInterleave(self, m: str, n: str, p: str) -> bool:
+        n1 = len(m)
+        n2 = len(n)
+        n3 = len(p)
+        cache = {}
+        def dfs(index1,index2,index):
+            nonlocal cache
+            if index1 == n1 and index2 == n2 and index == n3:
+                return True
+            if index == n3:
+                return False
+            key = (index1,index2,index)
+            if key not in cache:
+                countA,countB  = False,False
+                if index1 < n1 and m[index1] == p[index]:
+                    countA = dfs(index1+1,index2,index + 1)
+                if index2 < n2 and n[index2] == p[index]:
+                    countB = dfs(index1,index2 + 1,index + 1)
+                cache[key] = countA or countB
+
+            return cache[key]
+
+        return dfs(0,0,0)
