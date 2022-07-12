ad586af9fffe78d2f6bdaa9a18f7480574fcb474
Mrinal <dmrinal626@gmail.com>
2022-07-13 01:48:51 +0530

    Time: 42 ms (87.04%), Space: 13.9 MB (64.81%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,38 +1,23 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        
-				# def isFeasible(thresold):
-				# count = 1
-				# total = 0
-				# for num in nums:
-				#        total += num
-				# 			 if total > thres:
-				# 								 total = num
-				# 								 count += 1
-				# 								 if count >  thres:
-				# 											 return False
-				# 	return True
-        def feasible(thres):
-            count = 1
-            total = 0
-            for num in nums:
-                total += num
-                if total > thres:
-                    total = num
-                    count += 1
-                    if count > m:
-                        return False
-            return True
-        
         left = max(nums)
         right = sum(nums)
-        
+        n = len(nums)
+        def condition(maxSum):
+            totalN,count = 0,1
+            for i in range(n):
+                totalN += nums[i]
+                if totalN > maxSum:
+                    totalN = nums[i]
+                    count += 1
+                if count > m:
+                    return False
+            return True
+
         while left < right:
-            mid = left + (right - left)//2
-            if feasible(mid):
+            mid = left + (right - left) // 2
+            if condition(mid):
                 right = mid
             else:
                 left = mid + 1
-        
         return left
-                
