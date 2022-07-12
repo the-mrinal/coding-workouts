b2f47f187abdc246d0483e7d95f748cb7ffd67db
Mrinal <dmrinal626@gmail.com>
2022-07-13 01:17:04 +0530

    Time: 586 ms (77.62%), Space: 17.1 MB (80.97%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,29 +1,26 @@
+
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def isFeasible(cap):
-            total = 0
-            d = 1
-            for w in weights:
-                total += w
-                if total > cap:
-                    total = w
-                    d += 1
-                    if d > days:
-                        return False
-            return True
-
         left = max(weights)
         right = sum(weights)
+        n = len(weights)
+        # maximum capacity(C) where condition(C) is True
+        def condition(C):
+            totalC,totalD = 0,1
+            for i in range(n):
+                totalC += weights[i]
+                if totalC > C:
+                    totalD += 1
+                    totalC = weights[i]
+                if totalD > days:
+                    return False
+            return True
+
         while left < right:
-            mid = left + (right - left)//2
-            if isFeasible(mid):
+            mid = left + (right - left) // 2
+            if condition(mid):
                 right = mid
             else:
                 left = mid + 1
-        
+
         return left
-        
-        
-  
-			
-			
