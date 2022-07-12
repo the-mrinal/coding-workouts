f49b70add1891e745acedd08855f6b86c662f209
Mrinal <dmrinal626@gmail.com>
2022-07-13 02:19:13 +0530

    Time: 619 ms (64.64%), Space: 15.5 MB (74.79%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,28 +1,17 @@
-"""
-limits: 
-left = 1
-right = max(piles)
-
-isFeasible(x) # will check if koko will be able to complete all bananas within given hours if he eats x banana / hr
-
-def isFeasible(x):
-    return sum(ceil(pile/speed) for pile in piles) <= H
-
-
-
-"""
-
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def isFeasible(x):
-            return sum(ceil(pile/x) for pile in piles) <= h
         left = 1
         right = max(piles)
+        def condition(speed):
+            return sum([math.ceil(pile/speed) for pile in piles]) <= h
+
         while left < right:
-            mid = left + (right - left)//2
-            if isFeasible(mid):
+            mid = left + (right - left)// 2
+
+            if condition(mid):
                 right = mid
             else:
                 left = mid + 1
+
         return left
+
