936eb41e84fd51b4603e91e0bfe16b0252d788b5
Mrinal <dmrinal626@gmail.com>
2022-07-14 01:39:05 +0530

    Time: 161 ms (77.67%), Space: 14.9 MB (51.99%) - LeetHub

--- 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.py
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.py
@@ -5,15 +5,16 @@ class Solution:
         n = len(nums)
         nums.sort()
         # 1 3 7 8  10 18 23 24 78
-        def condition(distance):
-            count, i, j = 0, 0, 0
+        def condition(absDiff):
+            count = 0
+            i,j = 0,0
             while i < n or j < n:
-                while j < n and nums[j] - nums[i] <= distance:  # move fast pointer
+                while j < n and abs(nums[i] - nums[j]) <= absDiff:
                     j += 1
-                count += j - i - 1  # count pairs
-                i += 1  # move slow pointer
-            return count >= k
-
+                count += (j - i - 1)
+                i += 1
+            return count >= k 
+        
         while left < right:
             mid = left + (right - left) // 2
             
