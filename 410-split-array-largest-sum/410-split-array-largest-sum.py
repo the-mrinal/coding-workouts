971a3551e9d9d82a79114d105c51b8476ab15237
Mrinal <dmrinal626@gmail.com>
2022-07-14 00:14:43 +0530

    Time: 64 ms (44.94%), Space: 14 MB (64.81%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -2,22 +2,26 @@ class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
         left = max(nums)
         right = sum(nums)
-        n = len(nums)
-        def condition(maxSum):
-            totalN,count = 0,1
-            for i in range(n):
-                totalN += nums[i]
-                if totalN > maxSum:
-                    totalN = nums[i]
+        
+        def condition(currSum):
+            localSum,count = 0,1
+            for num in nums:
+                localSum += num
+                
+                if localSum > currSum:
+                    localSum = num
                     count += 1
+                
                 if count > m:
                     return False
             return True
-
+        
         while left < right:
             mid = left + (right - left) // 2
+            
             if condition(mid):
                 right = mid
             else:
                 left = mid + 1
+            
         return left
