693fde18061ca794e0d7c7e958534c859c3a06c5
Mrinal <dmrinal626@gmail.com>
2022-07-17 02:17:21 +0530

    Time: 75 ms (23.00%), Space: 14.1 MB (21.84%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,19 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        ans = []
-
-        def backtrack(st,end):
-            if st == end:
-                ans.append(nums[:])
-            for i in range(st,end):
-                nums[st],nums[i] = nums[i],nums[st]
-
-                backtrack(st + 1,end)
-
-                nums[st],nums[i] = nums[i], nums[st]
-
-
-        backtrack(0,len(nums))
-        
-        return ans
+        n = len(nums)
+        def permutationHelper(index):
+            nonlocal n
+            if index == n - 1:
+                return [[nums[index]]]
+            
+            perm = permutationHelper(index + 1)
+            new_perm = []
+            
+            for i in range(len(perm)):
+                for j in range(len(perm[i])+1):
+                    curr_p = perm[i][:j] + [nums[index]] + perm[i][j:]
+                    new_perm.append(curr_p)
+            return new_perm
+        return permutationHelper(0)
