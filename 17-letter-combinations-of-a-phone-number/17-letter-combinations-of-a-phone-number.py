64c18e950e4692db81e58b36e0402865ac13a393
Mrinal <dmrinal626@gmail.com>
2022-07-17 21:53:47 +0530

    Time: 77 ms (5.16%), Space: 14 MB (30.72%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -20,18 +20,48 @@ class Solution:
         
         '''
         n = len(digits)
-        def combinationHelper(index):
-            if index == n - 1:
-                return letterMap[int(digits[index])]
+#         def combinationHelper(index):
+#             if index == n - 1:
+#                 return letterMap[int(digits[index])]
             
-            comb = combinationHelper(index + 1)
-            size = len(comb)
-            new_comb = []
-            for i in range(size):
-                for elem in letterMap[int(digits[index])]:
-                    new_comb.append(elem + comb[i])
+#             comb = combinationHelper(index + 1)
+#             size = len(comb)
+#             new_comb = []
+#             for i in range(size):
+#                 for elem in letterMap[int(digits[index])]:
+#                     new_comb.append(elem + comb[i])
             
-            return new_comb
-        if n >= 1:
-            return combinationHelper(0)
-        return ""
+#             return new_comb
+#         if n >= 1:
+#             return combinationHelper(0)
+#         return ""
+    
+        if n == 0:
+            return ""
+        dp = []
+        prev_dp = letterMap[int(digits[-1])]
+        
+        for i in range(n-2,-1,-1):
+            currLetters = letterMap[int(digits[i])]
+            for elem in currLetters:
+                for i in range(len(prev_dp)):
+                    dp.append(elem + prev_dp[i])
+            prev_dp = dp.copy()
+            dp = []
+
+                
+        return prev_dp                    
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
