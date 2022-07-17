d3847489200ac10a58e5f01d0a857a64b77d2131
Mrinal <dmrinal626@gmail.com>
2022-07-18 00:40:08 +0530

    Time: 4434 ms (5.00%), Space: 489.6 MB (5.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,33 +1,20 @@
-# class Solution:
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-#         @lru_cache(None)
-#         def dp(sumLeft):
-#             if sumLeft < 0:
-#                 return float('inf')
-#             if sumLeft == 0:
-#                 return 0
-
-#             min_count = float(inf)
-#             for coin in coins:
-#                 res = 1 + dp(sumLeft - coin)
-#                 if res < min_count:
-#                     min_count = res
-#             return min_count
-        
-#         ans = dp(amount)
-#         return ans if ans < float('inf') else -1 
-    
-# 125 11
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-         
-            dp = [float('inf') for _ in range(amount + 1)]
-            dp[0] = 0
-            
-            for coin in coins:
-                for x in range(coin,amount + 1):
-                    dp[x] = min(dp[x],dp[x-coin] + 1)
-            
-            return dp[amount] if dp[amount] != float('inf') else -1
-                    
+    def coinChange(self, nums: List[int], amount: int) -> int:
+
+        n = len(nums)
+        cache = {}
+        def dp(index,target):
+            if (index >= n and target != amount) or target > amount:
+                return float('inf')
+            if target == amount:
+                cache[(index,target)] = 0
+                return cache[(index,target)]
+            key = (index,target)
+            if key not in cache:
+                countA = 1 + dp(index,target+ nums[index])
+                countB = dp(index+1,target)
+                cache[key] = min(countA,countB)
+
+            return cache[key]
+        dp(0,0)
+        return cache[(0,0)] if cache[0,0] < float('inf') else -1
