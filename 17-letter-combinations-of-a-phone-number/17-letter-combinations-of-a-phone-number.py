caafc20b6789a38ba4f364ecae7512875ff8bfa1
Mrinal <dmrinal626@gmail.com>
2022-07-17 21:43:50 +0530

    Time: 36 ms (82.63%), Space: 13.9 MB (78.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,29 +1,37 @@
 class Solution:
-    def letterCombinations(self, s: str) -> List[str]:
-        if not s:
-            return ""
-        keyMap = {
-            2: ['a','b','c'],
+    def letterCombinations(self, digits: str) -> List[str]:
+        letterMap = {
+            2:['a','b','c'],
             3:['d','e','f'],
             4:['g','h','i'],
-            5: ['j','k','l'],
-            6: ['m','n','o'],
-            7: ['p','q','r','s'],
-            8: ['t','u','v'],
-            9: ['w','x','y','z']
-            }
-
-        def recursion(s):
-            if len(s) == 1:
-                return keyMap[int(s[0])]
-
-            res = recursion(s[1:])	
-            curr_len  = len(res)
-            new_res = []
-            for k in keyMap[int(s[0])]:
-                for i in range(curr_len):
-                    new_res.append(k+res[i])
-
-            return new_res
+            5:['j','k','l'],
+            6:['m','n','o'],
+            7:['p','q','r','s'],
+            8:['t','u','v'],
+            9:['w','x','y','z'],
+        }
         
-        return recursion(s)
+        '''
+        23
+        
+        def
+        
+        ad,bd,cd,ae,be,ce bc
+        
+        '''
+        n = len(digits)
+        def combinationHelper(index):
+            if index == n - 1:
+                return letterMap[int(digits[index])]
+            
+            comb = combinationHelper(index + 1)
+            size = len(comb)
+            new_comb = []
+            for i in range(size):
+                for elem in letterMap[int(digits[index])]:
+                    new_comb.append(elem + comb[i])
+            
+            return new_comb
+        if n >= 1:
+            return combinationHelper(0)
+        return ""
