78e6fbadac2035c50e3e72488d71a7bcaeb99020
Mrinal <dmrinal626@gmail.com>
2022-07-17 23:44:52 +0530

    Time: 48 ms (52.66%), Space: 13.9 MB (65.75%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,65 +1,22 @@
-
-# class Solution:
-#     def rob(self, nums: List[int]) -> int:
-#         n = len(nums)
-#         if n == 1:
-#             return nums[0]
-#         if n == 2:
-#             return max(nums[0],nums[1])
-        
-        
-#         def dp(i):
-#             if i == 0:
-#                 return nums[i]
-#             if i == 1:
-#                 return max(nums[0],nums[1])
-            
-            
-#             if i not in memo:
-#                 memo[i] = max(dp(i-1),dp(i-2)+nums[i])
-            
-#             return memo[i]
-        
-#         memo = {}
-#         dp(n - 1)
-        
-#         return memo[n- 1]
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        '''
+        1 2 3 1
+        dp [ max profit till house i]
+        dp[0] = nums[0]
+        dp[1] = max(nums[i],nums[i - 1])
         
+        dp[3] = max(nums[i] + dp[i - 2] , dp[i - 1])
+        
+        '''
         n = len(nums)
-        
-        
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            return max(nums[0],nums[1])
         dp = [0 for _ in range(n)]
+        if n <= 2:
+            return max(nums)
         
         dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        
+        dp[1] = max(nums[:2])
         for i in range(2,n):
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
-            
-        return dp[n-1]
+            dp[i] = max(nums[i] + dp[i - 2],dp[i-1])
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return dp[n - 1]
