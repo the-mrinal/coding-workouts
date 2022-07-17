d988bf78da640c32b9c0e370b0e7dd44aa98cf16
Mrinal <dmrinal626@gmail.com>
2022-07-17 20:24:32 +0530

    Time: 29 ms (96.18%), Space: 13.9 MB (68.44%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        n = len(s)
+        def validate(s,index,CURR):
+            bal ,wild =0,0
+            for i in range(n):
+                if s[i] != "*":
+                    bal += 1 if s[i] == CURR else -1
+                else:
+                    wild += 1
+                if wild + bal < 0:
+                    return False
+            return wild >= bal
+        return validate(s,0,'(') and validate(s[::-1],0,')')
