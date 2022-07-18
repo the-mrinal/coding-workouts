b987a377469350ffefea38e894d85a20e842708e
Mrinal <dmrinal626@gmail.com>
2022-07-19 02:00:10 +0530

    Time: 7050 ms (5.00%), Space: 13.9 MB (92.24%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -7,14 +7,14 @@ class Solution:
     
     def KSumSubSeq(self,nums,K):
         n = len(nums)
-        dp = [[0 for _ in range(K + 1)] for _ in range(n)]
+        dp = [[0 for _ in range(K + 1)] for _ in range(2)]
         
         for i in range(K + 1):
             dp[0][i] = True if i == nums[0] else False
         for i in range(1,n):
             for j in range(K+1):
                 if nums[i] <= j:
-                    dp[i][j] = dp[i - 1][j - nums[i]]
-                dp[i][j] = dp[i][j] or dp[i - 1][j]
+                    dp[i%2][j] = dp[(i - 1)%2][j - nums[i]]
+                dp[i%2][j] = dp[i%2][j] or dp[(i - 1)%2][j]
         
-        return dp[n - 1][K]
+        return dp[(n - 1)%2][K]
