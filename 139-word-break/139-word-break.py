278efdc772025d1bc62afda92312fcec58346ee8
Mrinal <dmrinal626@gmail.com>
2022-07-18 19:31:40 +0530

    Time: 39 ms (93.62%), Space: 14 MB (71.00%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,12 +1,34 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        dp = [False for _ in range(n)]
+    def wordBreak(self, s: str, words: List[str]) -> bool:
+        '''
         
-        for i in range(n):
-            for word in wordDict:
-                if ((len(word) - 1 <= i) and (len(word) == i + 1 or dp[i - len(word)])):
-                    if s[i - len(word)+1:i+1] == word:
+        #leetcode
+        #['leet','code']
+        n = len(s)
+        dp = [False] * n
+        
+        
+        for i in range(n): # n is len of s
+            for word in words:
+                if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
+                    if s[i - len(word) + 1:i+1] == word:
                         dp[i] = True
                         break
+        
+        return dp[n - 1]
+        
+        
+        '''
+        
+        n = len(s)
+        dp = [False] * n
+        
+        
+        for i in range(n): # n is len of s
+            for word in words:
+                if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
+                    if s[i - len(word) + 1:i+1] == word:
+                        dp[i] = True
+                        break
+        
         return dp[n - 1]
