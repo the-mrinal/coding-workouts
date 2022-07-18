8aa9c5d86814b314a8658ead4922e05daa8ab635
Mrinal <dmrinal626@gmail.com>
2022-07-19 00:37:42 +0530

    Time: 171 ms (69.73%), Space: 13.9 MB (77.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,20 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
         n = len(s)
-        new_s = s
-
         def helper(left,right):
-            ans = 0 
-            while left > -1 and right < n and new_s[left] == new_s[right]:
-                ans = ans + 1
+            currMax = 0
+            while left >= 0 and right < n:
+                if s[left] == s[right]:
+                    currMax += 1
+                else:
+                    break
                 left -= 1
                 right += 1
-            return ans
-                    
-        
+            return currMax
+        total = 0
         for i in range(n):
             a = helper(i,i)
-            b = helper(i,i +1)
-            count += a + b
-        return count
+            b = helper(i,i+1)
+            total += a + b
+        return total
