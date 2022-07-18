f3a3286871d7a7e2f31995f2051395d65453ab5a
Mrinal <dmrinal626@gmail.com>
2022-07-18 21:56:46 +0530

    Time: 443 ms (15.26%), Space: 22 MB (14.06%) - LeetHub

--- 320-generalized-abbreviation/320-generalized-abbreviation.py
+++ 320-generalized-abbreviation/320-generalized-abbreviation.py
@@ -6,24 +6,29 @@ class Substring:
 
 class Solution:
     def generateAbbreviations(self, word: str) -> List[str]:
-        que = deque()
-        que.append(Substring())
+        
         n = len(word)
         result = []
+        que = deque()
+        que.append(Substring())
+        
         while que:
             curr_ab = que.popleft()
+            
             if curr_ab.start == n:
                 if curr_ab.count != 0:
                     curr_ab.s.append(str(curr_ab.count))
                 result.append(''.join(curr_ab.s))
+            
             else:
-                que.append(Substring(list(curr_ab.s),curr_ab.start + 1,curr_ab.count + 1))  # continue abb
+    
+                que.append(Substring(list(curr_ab.s),curr_ab.start + 1,curr_ab.count + 1)) # abb form
                 
                 if curr_ab.count != 0:
                     curr_ab.s.append(str(curr_ab.count))
-                    
-                new_w = list(curr_ab.s)
-                new_w.append(word[curr_ab.start])
-                que.append(Substring(new_w,curr_ab.start + 1,0))
+                
+                new_word = list(curr_ab.s)
+                new_word.append(word[curr_ab.start])
+                que.append(Substring(new_word,curr_ab.start + 1,0))
+        
         return result
-            
