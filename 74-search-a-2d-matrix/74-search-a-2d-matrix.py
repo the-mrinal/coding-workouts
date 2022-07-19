785636ea2f6b47b47904a13ed92f5f7b892aef3c
Mrinal <dmrinal626@gmail.com>
2022-07-20 02:54:06 +0530

    Time: 50 ms (85.26%), Space: 14.4 MB (89.03%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -3,31 +3,24 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
-        total = m * n
+        total = m*n
         
-        # n*col + row -> [row][col]
-        # given x -> x / col <- curr_col and x&col <- curr_row
+        left = 0
+        right = total
         
-        si = 0
-        ei = total
-        
-        def isFeasible(mid):
-            col ,row = mid % n , mid // n
+        def condition(mid):
+            row,col = mid // n,mid % n
             if matrix[row][col] >= target:
                 return True
             return False
         
-        
-        while si < ei:
-            mid = si + (ei - si)//2
+        while left < right:
+            mid = left + (right - left) // 2
             
-            if isFeasible(mid):
-                ei = mid
+            if condition(mid):
+                right = mid
             else:
-                si = mid + 1
-        
-        
-        col ,row = si % n , si // n
-        return True if row < m and col < n and matrix[row][col] == target else False
-        
-        
+                left = mid + 1
+        row = left // n
+        col = left % n
+        return True if row < m and col < n and matrix[row][col] == target else False 
