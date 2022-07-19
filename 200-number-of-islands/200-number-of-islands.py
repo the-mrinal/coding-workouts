810c326b3efc387c05f72f8e262630a7c353a9f3
Mrinal <dmrinal626@gmail.com>
2022-07-19 21:43:27 +0530

    Time: 401 ms (71.34%), Space: 20.6 MB (35.43%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,47 +1,58 @@
 class UnionFind:
-    def __init__(self,grid):    
-        m = len(grid)
-        n = len(grid[0])
-        self.count = sum(grid[i][j]=='1' for i in range(m) for j in range(n))
-        self.root = [i for i in range(m*n)]
-        self.rank = [0 for _ in range(m*n)]
-
+    
+    def __init__(self,grid):
+        rowSize = len(grid)
+        colSize = len(grid[0])
+        self.count = sum([sum([int(grid[i][j]) for j in range(colSize)]) for i in range(rowSize)])
+        self.root = [i for i in range(rowSize*colSize)]
+        self.rank = [1 for i in range(rowSize * colSize)]
+    
     def find(self,x):
-        if x == self.root[x]:
+        if self.root[x] == x:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
-
+    
     def union(self,a,b):
         rootA = self.find(a)
         rootB = self.find(b)
-
+        
         if rootA != rootB:
             if self.rank[rootA] > self.rank[rootB]:
-                    self.root[rootB] = rootA
-            elif self.rank[rootA] < self.rank[rootB]:
-                    self.root[rootA] = rootB
+                self.root[rootB] = rootA
+            elif self.rank[rootB] > self.rank[rootA]:
+                self.root[rootA] = rootB
             else:
                 self.root[rootB] = rootA
                 self.rank[rootA] += 1
-            self.count -= 1
-	
+            self.count -= 1        
+
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        
+        rowS = len(grid)
+        colS = len(grid[0])
+        
         uf = UnionFind(grid)
-        directions = [[1,0],[0,1]]
-        print(uf.count)
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '0':
-                    continue
-                index = i*n + j
-                if j < n-1 and grid[i][j+1] == '1':
-                    uf.union(index, index+1)
-                if i < m-1 and grid[i+1][j] == '1':
-                    uf.union(index, index+n)
-
-        print(uf.count)
+        
+        
+        for i in range(rowS):
+            for j in range(colS):
+                index = i *colS + j
+                if grid[i][j] == '1':
+                    if j < colS - 1 and grid[i][j + 1] == '1':
+                        uf.union(index,index + 1)
+                    if i < rowS - 1 and grid[i+1][j] == '1':
+                        uf.union(index,index + colS)
         return uf.count
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
