fe01609669fb0a85c75cdc6bd991fc7c80553216
Mrinal <dmrinal626@gmail.com>
2022-07-27 04:12:14 +0530

    Time: 509 ms (54.31%), Space: 32.1 MB (38.22%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,27 +1,21 @@
 class Solution:
-
     def find132pattern(self, nums: List[int]) -> bool:
-        n = len(nums)
-        # here we want to compare - nums-i < nums-k < nums-j
-        # keep track of max num till index i
-        # ?? how to keep track of mins ?
         stack = []
-        
         min_arr = [nums[0]]
+        n = len(nums)
         for i in range(1,n):
             min_arr.append(min(min_arr[i - 1],nums[i]))
-
-        # [1,1,1,1]
-        st = []
+        
         for i in range(n - 1,0,-1):
             if nums[i] <= min_arr[i]:
                 continue
-
-            while st and st[-1] <= min_arr[i]:
-                st.pop()
-            if st and st[-1] < nums[i]:
+                
+            while stack and stack[-1] <= min_arr[i]:
+                stack.pop()
+            
+            if stack and stack[-1] < nums[i]:
                 return True
-
-            st.append(nums[i])
-
+            
+            stack.append(nums[i])
+        
         return False
