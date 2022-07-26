914dbbd83b56ab9aa2fa7105d0ad138605f9cbe9
Mrinal <dmrinal626@gmail.com>
2022-07-27 02:17:34 +0530

    Time: 60 ms (25.05%), Space: 14.2 MB (5.01%) - LeetHub

---
+++ 677-map-sum-pairs/677-map-sum-pairs.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.sum = 0
+
+
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
+        self.map = defaultdict(int)
+
+    
+    def insert(self, key: str, val: int) -> None:
+        curr = self.root
+        diff = val - self.map[key]
+        for c in key:
+            curr = curr.children[c]
+            curr.sum += diff
+            print(curr.sum,c)
+        self.map[key] = val
+
+    def sum(self, prefix: str) -> int:
+        curr = self.root
+        
+        for c in prefix:
+            if c not in curr.children: return 0
+            curr = curr.children[c]
+    
+        return curr.sum
+        
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
