bcbf96e4d69c93d6ba7afe07d11baf98b003adde
Mrinal <dmrinal626@gmail.com>
2022-07-28 02:10:25 +0530

    Time: 38 ms (78.40%), Space: 13.8 MB (98.26%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
@@ -4,40 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-# #         Time = O(n^2) <- bad
-#         curr = root
-#         ans = []
-#         while root:
-#             if not root.left and not root.right:
-#                 ans.append([root.val])
-#                 root = None
-#                 continue
-#             ans.append([])
-#             que = deque([[root,None,-1]])
-#             while que:
-#                 curr,curr_root,side = que.popleft()
-#                 if not curr.left and not curr.right:
-#                     ans[-1].append(curr.val)
-#                     if side == 1:
-#                         curr_root.left = None
-#                     elif side == 0:
-#                         curr_root.right = None
-#                     continue
-
-#                 if curr.right:
-#                     que.append([curr.right,curr,0])
-#                 if curr.left:
-#                     que.append([curr.left,curr,1])
-            
-#         return ans
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         time = O(N) 
         ans = defaultdict(list)
+        
         def dfs(node,layer):
-            if not node:
+            if node is None:
                 return layer
             left = dfs(node.left,layer)
             right = dfs(node.right,layer)
@@ -45,35 +17,8 @@ class Solution:
             layer = max(left,right)
             
             ans[layer].append(node.val)
-            
-            return layer + 1
+            layer += 1
+            return layer
         
         dfs(root,0)
         return ans.values()
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
