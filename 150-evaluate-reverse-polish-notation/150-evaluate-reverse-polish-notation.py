174bc78eb8b303536cb33cc98e1f99d4881f0b36
Mrinal <dmrinal626@gmail.com>
2022-07-28 23:54:57 +0530

    Time: 120 ms (34.14%), Space: 14.5 MB (5.74%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,27 +1,31 @@
+'''
+["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+10 -1 
+
+'''
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        if len(tokens) == 1:
-            return tokens[0]
-        def operation(left,right,token):
-            left = int(left)
-            right = int(right)
-            if token == '+':
-                return left + right
-            elif token == '-':
-                return left - right
-            elif token == '*':
-                return left * right
-            elif token == '/':
-                return int(left / right)
-        ops = ['/','+','-','*']
         stack = []
-        res = None
-        for token in tokens:
-            if token in ops:
-                if res is None:
-                    right = stack.pop()
-                    left = stack.pop()
-                    stack.append(operation(left,right,token))
+        operations = ['+','-','*','/']
+        
+        def operate(left,right,op):
+            if(op == '+'):
+                return left + right
+            if(op == '-'):
+                return left - right
+            if(op == '*'):
+                return left * right
+            if(op == '/'):
+                return int(left / right)
+        
+        for char in tokens:
+            if char in operations:
+                right = stack.pop()
+                left = stack.pop()
+                curr = operate(left,right,char)
+                stack.append(curr)
             else:
-                stack.append(int(token))
+                stack.append(int(char))
+        
         return stack.pop()
